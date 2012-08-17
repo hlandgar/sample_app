1fa23cc9d5c0b677446d045016545e1afc865bfd
Harris Landgarten <harris@tradeanedge.com>
2012-08-17 19:45:14 -0400

    Add user edit and index actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:edit, :update, :index]
+  before_filter :correct_user, only: [:edit, :update]
+
   def new
   	@user = User.new
+  end
+
+  def index
+    @users = User.all
   end
 
   def show
@@ -16,6 +23,33 @@ class UsersController < ApplicationController
   	else
   		render 'new'
   	end
-  	
   end
-end
+
+  def edit
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      sign_in @user
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  private
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_path, notice: "Please sign in."
+      end
+      
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to root_path unless current_user?(@user)
+    end
+ end
